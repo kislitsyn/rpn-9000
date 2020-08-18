70d1c9cdb4a0a547709d724634510f89fbfe5223
Anton Kislitcyn <anton_kislitcyn@epam.com>
2020-08-18 23:57:29 +0200

    Initial project structure configured

---
+++ cli/src/main/java/com/rpn/calculator/cli/Application.java
@@ -0,0 +1,13 @@
+package com.rpn.calculator.cli;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        log.info("RPN 9000 has been started");
+    }
+}
